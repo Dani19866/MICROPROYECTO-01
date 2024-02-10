d822c9f787d2f04e403e47f6882f9815d693a84d
Danielito <daniel.deoliveira@correo.unimet.edu.ve>
2024-02-10 02:25:17 -0400

    ✍️ GAME V1.0

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,18 @@
-// Variables in HTML document Menu/
+// Global
+const CONTROLLER = document.getElementById("controller")
+
+// Variables in Menu
 const GAMES_HISTORIAL_BUTTON = document.getElementById("seeHistorial");
 const RETURN_MENU_ICON_BUTTON = document.getElementById("returnMenu")
 const GAME_FORM = document.getElementById("form_game")
+const MENU_CONTROLLER = document.getElementById("menuController")
+
+// Variables in Game
+const GAME_CONTROLLER = document.getElementById("gameController")
+const IDPLAYER = document.getElementById("idPlayer")
+const HISTORIALMOVEMENTS = document.getElementById("historialMovements")
+const GIVE_NUMBER_BUTTON = document.getElementById("giveNumber")
+const CHANGE_PLAYER_BUTTON = document.getElementById("changePlayer")
 
 GAMES_HISTORIAL_BUTTON.addEventListener("click", (e) => {
     controller = false
@@ -24,6 +35,13 @@ RETURN_MENU_ICON_BUTTON.addEventListener("click", (e) => {
 })
 
 GAME_FORM.addEventListener("submit", (e) => {
+    // Reset behavior
     e.preventDefault()
-    console.log(e)
+
+    // Changes state
+    MENU_CONTROLLER.style.display = "none"
+    GAME_CONTROLLER.style.display = "block"
+    CONTROLLER.className = "";
+
+    // Logic
 })
