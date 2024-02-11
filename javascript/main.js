00688fd4563c443a4df83c5c876a04d8ba9dd0c4
Danielito <daniel.deoliveira@correo.unimet.edu.ve>
2024-02-11 01:10:28 -0400

    🔒 Game Finish. Version V1.0.0

--- javascript/main.js
+++ javascript/main.js
@@ -21,7 +21,7 @@ const GIVE_NUMBER_BUTTON = document.getElementById("giveNumber")
 const RESTART_BUTTON = document.getElementById("restart")
 const LEAVE_BUTTON = document.getElementById("leave")
 const TABLE_PLAYERS = document.getElementById("tablesPlayers")
-
+const SCORES_HISTORIAL = document.getElementById("scores")
 
 // ------------------------------ | ------------------------------
 // Eventos
@@ -29,10 +29,11 @@ document.addEventListener("DOMContentLoaded", (e) => {
     for (let i = 1; i <= 50; i++) {
         numbers.push(i);
     }
+
+    update_score_in_html()
 })
 
 GAMES_HISTORIAL_BUTTON.addEventListener("click", (e) => {
-    controller = false
     // Logic
     let reg_container = document.getElementById("reg")
     let score_container = document.getElementById("sco")
@@ -86,7 +87,33 @@ GAME_FORM.addEventListener("submit", (e) => {
 })
 
 GIVE_NUMBER_BUTTON.addEventListener("click", (e) => {
-    update_matriz(extract_number());
+    if (movements <= 25) {
+        update_matriz(extract_number());
+    }
+    else {
+        // Message
+        alert("La partida ha llegado a su máximo de fichas (25), se procederá a guardar los resultados en la base de datos, puedes acceder a ella a través del menú. Buena suerte para la próxima!")
+
+        // Save data
+        save_points()
+
+        // Update score
+        reset_scores_in_html(1)
+        update_score_in_html()
+
+        // Reset game
+        finish_game()
+    }
+})
+
+LEAVE_BUTTON.addEventListener("click", (e) => {
+    alert("Saliste del juego. AVISO: No se guardarán los datos")
+    finish_game()
+})
+
+RESTART_BUTTON.addEventListener("click", (e) => {
+    alert("Se reinició el juego. Los números y las tablas fueron reiniciadas!")
+    reset_game()
 })
 
 // ------------------------------ | ------------------------------
@@ -179,22 +206,216 @@ function update_matriz(number) {
 }
 
 function add_historial(number) {
-    if (movements <= 25) {
-        let historialHTML = '<tr>'
-        historialHTML += '<td class="reset_table_component">' + movements + '</td>'
-        historialHTML += '<td class="reset_table_component">' + number + '</td>'
-        historialHTML += '</tr>'
+    let historialHTML = '<tr>'
+    historialHTML += '<td class="reset_table_component">' + movements + '</td>'
+    historialHTML += '<td class="reset_table_component">' + number + '</td>'
+    historialHTML += '</tr>'
 
-        HISTORIALMOVEMENTS.innerHTML += historialHTML
-        movements += 1
-    }
-    else {
-        alert("SE HA ACABADO LA PARTIDA!")
-    }
+    HISTORIALMOVEMENTS.innerHTML += historialHTML
+    movements += 1
 }
 
 function extract_number(arr) {
     const indiceAleatorio = Math.floor(Math.random() * numbers.length);
     const numeroAleatorio = numbers.splice(indiceAleatorio, 1)[0];
     return numeroAleatorio;
+}
+
+function reset_historial() {
+    HISTORIALMOVEMENTS.innerHTML = ""
+}
+
+function reset_game() {
+    // Reset numbers
+    {
+        numbers = []
+        movements = 1
+        for (let i = 1; i <= 50; i++) {
+            numbers.push(i);
+        }
+    }
+
+    // Reset players information
+    {
+        let namePlayers = Object.keys(players_info_menu)
+        for (let m = 0; m < namePlayers.length; m++) {
+            const name = namePlayers[m];
+            const size = players_info_menu[name]
+
+            for (let i = 0; i < size; i++) {
+                for (let j = 0; j < size; j++) {
+                    players_table_controller[name][i][j] = false
+                }
+            }
+        }
+    }
+
+    // Update interface
+    {
+        reset_matriz_interface()
+        let namePlayers = Object.keys(players_info_menu)
+        for (let i = 0; i < namePlayers.length; i++) {
+            const name = namePlayers[i];
+            const matriz_number = players_table[name]
+            const matriz_controller = players_table_controller[name]
+            add_matriz_into_table(name, matriz_number, matriz_controller)
+        }
+        reset_historial()
+    }
+}
+
+function finish_game() {
+    // Reset numbers
+    {
+        numbers = []
+        movements = 1
+        for (let i = 1; i <= 50; i++) {
+            numbers.push(i);
+        }
+    }
+
+    // Delete players and information
+    {
+        players_info_menu = {}
+        players_table = {}
+        players_table_controller = {}
+    }
+
+    // Update interface
+    {
+        MENU_CONTROLLER.style.display = "block"
+        GAME_CONTROLLER.style.display = "none"
+        CONTROLLER.className = "center"
+        reset_matriz_interface()
+        reset_historial()
+    }
+}
+
+function save_points() {
+    // 1. Extraer historial de puntos
+    let currentScores = JSON.parse(localStorage.getItem('scores')) || {};
+
+    // 2. Calcular puntajes nuevos y sobreescribir viejos
+    let calcScores = calc_score()
+
+    // 3. Concatenar objetos
+    let namePlayers = Object.keys(calcScores)
+    for (let m = 0; m < namePlayers.length; m++) {
+        const name = namePlayers[m];
+        if (currentScores[name]){
+            currentScores[name] += calcScores[name]
+        }
+        else{
+            currentScores[name] = calcScores[name]
+        }
+    }
+
+    console.log(currentScores);
+
+    localStorage.setItem('scores', JSON.stringify(currentScores));
+}
+
+function calc_score() {
+    let scores = {}
+
+    let namePlayers = Object.keys(players_info_menu)
+    for (let m = 0; m < namePlayers.length; m++) {
+        const name = namePlayers[m];
+        const matriz_controller = players_table_controller[name]
+
+        let diagonal = 0
+        let horizontal = 0
+        let vertical = 0
+        let full = 1
+
+        // Patterns logic
+        {
+            // Verificar diagonal
+            for (let i = 0; i < matriz_controller.length; i++) {
+                if (matriz_controller[i][i] == false) {
+                    diagonal = 0;
+                    break;
+                }
+                diagonal += 1;
+            }
+
+            // Verificar horizontal
+            for (let i = 0; i < matriz_controller.length; i++) {
+                x = true
+
+                for (let j = 0; j < matriz_controller.length; j++) {
+                    if (matriz_controller[j][i] == false) {
+                        x = false
+                    }
+                }
+
+                if (x) {
+                    horizontal += 1
+                }
+            }
+
+            // Verificar vertical
+            for (let i = 0; i < matriz_controller.length; i++) {
+                x = true
+
+                for (let j = 0; j < matriz_controller.length; j++) {
+                    if (matriz_controller[i][j] == false) {
+                        x = false
+                    }
+                }
+
+                if (x) {
+                    vertical += 1
+                }
+            }
+
+            // Verificar cartón lleno
+            for (let i = 0; i < matriz_controller.length; i++) {
+                for (let j = 0; j < matriz_controller.length; j++) {
+                    if (matriz_controller[i][j] == false) {
+                        full = 0
+                    }
+                }
+            }
+        }
+
+        // Patterns points
+        {
+            if (full == 1) {
+                scores[name] += full * 10
+            }
+            else {
+                scores[name] = (diagonal * 5) + (horizontal * 3) + (vertical * 3)
+            }
+        }
+
+    }
+
+    return scores
+}
+
+function reset_scores_in_html() {
+    SCORES_HISTORIAL.innerHTML = ""
+}
+
+function update_score_in_html() {
+    /* 
+        <tr>
+            <td>Usuario 1</td>
+            <td>100</td>
+        </tr>
+    */
+    const currentScores = JSON.parse(localStorage.getItem('scores')) || {};
+    let playersName = Object.keys(currentScores)
+    for (let i = 0; i < playersName.length; i++) {
+        const name = playersName[i];
+        const score = currentScores[name]
+
+        let historialHTML = '<tr>'
+        historialHTML += '<td>' + name + '</td>'
+        historialHTML += '<td>' + score + '</td>'
+        historialHTML += '</tr>'
+
+        SCORES_HISTORIAL.innerHTML += historialHTML
+    }
 }
